012a6ff01a1a900a4e9fc24acf168089459bf710
sowTecsup <jneyra@tecsup.edu.pe>
2025-10-24 12:28:29 -0500

    UpdatyeEnemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,13 @@ public class Enemy : MonoBehaviour
     public GameObject target;
 
     [SerializeField] private float speed;
+    [SerializeField] private float MovementRange;
+    [SerializeField] private float attackRange = 0.5f;
+
+    [SerializeField] private Vector3 initialPos;
     void Start()
     {
-        
+        initialPos = transform.position;
     }
 
     void Update()
@@ -16,9 +20,30 @@ public class Enemy : MonoBehaviour
     }
     public void MoveToTarget()
     {
-        Vector3 dir = target.transform.position - transform.position;//Normalizada
-        Vector3 normalizedDir = dir.normalized;
 
-        transform.position += normalizedDir * speed * Time.deltaTime;
+        if(Vector2.Distance(transform.position, target.transform.position) <= MovementRange)
+        {
+            if (Vector2.Distance(transform.position, target.transform.position) <= attackRange)
+            {
+                print("Atacando al player");
+            }
+            else
+            {
+                Vector3 dir = target.transform.position - transform.position;//Normalizada
+                Vector3 normalizedDir = dir.normalized;
+
+                transform.position += normalizedDir * speed * Time.deltaTime;
+            }
+
+               
+        }
+        else
+        {
+            Vector3 dir = initialPos - transform.position;//Normalizada
+            Vector3 normalizedDir = dir.normalized;
+            transform.position += normalizedDir * speed * Time.deltaTime;
+        }
+
+     
     }
 }
